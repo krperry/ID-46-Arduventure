d2f2bf2a3a11b2467d71c06dab90cad501e4fbe2
Joeri Gantois <info@JO3RI.be>
2016-05-14 15:50:35 +0200

    switched from drawBitmap to sprites.drawSelfMasked

--- DUMMY_AB/menu.h
+++ DUMMY_AB/menu.h
@@ -7,13 +7,13 @@
 void stateMenuIntro()
 {
   counter++;
-  arduboy.drawBitmap(0, 8, TEAMarg, 128, 48, WHITE);
+  sprites.drawSelfMasked(0, 8, TEAMarg, 0);
   if (counter > 180) gameState = STATE_MENU_MAIN;
 }
 
 void stateMenuMain()
 {
-  arduboy.drawBitmap(0, 0, titleScreen, 128, 64, WHITE);
+  sprites.drawSelfMasked(0, 0, titleScreen, 0);
   for (byte i = 0; i < 4; i++)
   {
     {
@@ -31,14 +31,14 @@ void stateMenuMain()
 
 void stateMenuHelp()
 {
-  arduboy.drawBitmap(32, 0, qrcode, 64, 64, WHITE);
+  sprites.drawSelfMasked(32, 0, qrcode, 0);
   if (buttons.justPressed(A_BUTTON | B_BUTTON)) gameState = STATE_MENU_MAIN;
 }
 
 
 void stateMenuInfo()
 {
-  arduboy.drawBitmap(16, 20, info, 96, 24, WHITE);
+  sprites.drawSelfMasked(16, 20, info, 0);
   if (buttons.justPressed(A_BUTTON | B_BUTTON)) gameState = STATE_MENU_MAIN;
 }
 
